68c22439ec8442bc147e72c6556f498fc65c10e6
magicmmp <2233973404@qq.com>
2020-06-10 00:08:02 +0800

    auto git push hhh VisualStudio_Projects: 2020年06月10日  0:08:02

---
+++ 类和接口的继承_练习/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 类和接口的继承_练习
+{
+    class MyBaseClass
+    {
+        public int index;
+        
+    }
+
+    class MyBaseClass2
+    {
+        public int index;
+        
+    }
+}
---
+++ 类的访问修饰符2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLibrary;
+
+namespace 类和接口的继承_练习
+{
+    class MyBaseClass
+    {
+        public int index;
+
+    }
+
+    class MyBaseClass2
+    {
+        public int index;
+
+    }
+}
+
+namespace 类的访问修饰符2
+{
+    class Program: 类和接口的继承_练习.MyBaseClass2
+    {
+        static void Main(string[] args)
+        {
+            MyExternalClass myExternalClass = new MyExternalClass();
+            myExternalClass.i = 10;
+            Console.WriteLine("i={0}",myExternalClass.i);
+            Console.ReadKey();
+        }
+    }
+}

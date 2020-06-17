a641fd2c7ca0b7e6d8d0b3cf45ce21f2b6f23b5c
magicmmp <2233973404@qq.com>
2020-06-17 23:49:51 +0800

    auto git push hhh VisualStudio_Projects: 2020年06月17日 23:49:50

---
+++ WinForm练习/TrackBar/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace TrackBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.labelTrackBarValue = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.trackBar2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.trackBar2.Location = new System.Drawing.Point(338, 140);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar2.Size = new System.Drawing.Size(56, 326);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar2.ValueChanged += new System.EventHandler(this.trackBar2_ValueChanged);
+            // 
+            // labelTrackBarValue
+            // 
+            this.labelTrackBarValue.AutoSize = true;
+            this.labelTrackBarValue.BackColor = System.Drawing.SystemColors.Info;
+            this.labelTrackBarValue.Location = new System.Drawing.Point(433, 303);
+            this.labelTrackBarValue.Name = "labelTrackBarValue";
+            this.labelTrackBarValue.Size = new System.Drawing.Size(15, 15);
+            this.labelTrackBarValue.TabIndex = 2;
+            this.labelTrackBarValue.Text = "0";
+            this.labelTrackBarValue.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 725);
+            this.Controls.Add(this.labelTrackBarValue);
+            this.Controls.Add(this.trackBar2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label labelTrackBarValue;
+    }
+}
+

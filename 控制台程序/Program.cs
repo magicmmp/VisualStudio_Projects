067f57a85004f1d783bb2868cc18242e80b555d7
magicmmp <2233973404@qq.com>
2020-06-24 18:18:13 +0800

    auto git push hhh VisualStudio_Projects: 2020-06-24 18:18:12

---
+++ 控制台程序/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 控制台程序
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("输出点什么");
+            Console.ReadKey();
+        }
+    }
+}
